b8f4def2e269e7b3726a31d6ca1921b03070f7e3
Lukas Michalek <luke.michalek@gmail.com>
2026-01-24 19:47:23 +0100

    chore(user): store event emitter logic backup
    Saved a backup snippet of the current user selection
    implementation before further refactoring.
    
    The snippet contains the full child-to-parent
    communication flow:
    - button click handler in UserComponent
    - onSelectUser method emitting the user id via
      EventEmitter
    - parent AppComponent listener and handler method
    - console logging of the received user id
    
    All relevant code is commented to explain the event
    flow, component interaction, and data propagation for
    learning and future reference.

---
+++ Code-Snippets/01-first-refresher-angular-app-snippets/35. Working with Outputs & Emitting Data/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+
+import { DUMMY_USERS } from './DUMMY_USERS';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+
+
+
+// * 4. When the user clicks a button Event Listener <button (click)="onSelectUser()"> calls the onSelectUser() function located in user.componenet.ts (becasue it is that component that triggers it). This function then emits the value that was sent (in this case user ID) thanks to EventEmitter object that is stored in 'select' property (to create this property and new EventEmitter I need Output decorator). This select property then emits this information (id) to its parent (that is app.component.html) where there is a custom listener (select)="onSelectUser($event) waiting for the change and triggers another EventListener that triggers onSelectUser function located in app.component.ts*
+
+
+
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string){
+
+    console.log('Selected user with id' + id)
+
+  }
+
+
+}
---
+++ Code-Snippets/01-first-refresher-angular-app-snippets/35. Working with Outputs & Emitting Data/app/user/user.component.ts
@@ -0,0 +1,58 @@
+//  * 1. *  35. Working with Outputs & Emitting Data  **  //
+
+// - Note that I will be working with Management State Change rather that signals here
+
+// + The goal here is that after I click a user (user.component.html) I want to show their tasks. This task list will not show inside user component because the user element is only responsoble for outputing the user element (button on UI with avatar and name). Therefore the information that hte user was clicked must get out of the user component and must be passed on to the app component (app.component.ts) because it is the app component that is outputting the user and it is the app component that could then output the tasks of that user nexto to that unordered list in app.component.html
+
+// # So we need to let the app component know when a user is clicked so when the button in the user component is clicked.
+
+// * There is already an event listener in user.component.html ===> <button (click)="onSelectUser()"> but I need to OUTPUT this information that this button was clicked. I will do that with Angular output properties
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+
+
+// * To set up the output property I need to add another regular property I can call it according to the function. The name should describe the custom event I plan on emitting, and because I want to pass the information that a specific user was selected to the component that`s using the UserComponent
+
+// - The select componenet must be decorated with another decorator that is imported from Angular => the Output decorator. Imported like Input decorator and used just like the input.
+
+// # Unlike the input, the select property will receive an initial value. This initial value is a 'new EventEmitter object', where EventEmitter is actually also imported from Angular Core
+
+// + This EventEmitter object ==> @Output select = new EventEmitter(); ==> will then allow me to emit custom values through that select property to any parent component (in this cas app.component) that is interested 
+
+
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  @Output() select = new EventEmitter(); 
+
+  get imagePath() {
+    return '../../assets/users/' + this.avatar;
+  }
+
+  
+  
+  // * When user clicks a button, Event Listener calls a onSelectUser() function ===> <button (click)="onSelectUser()">   ===> the select property that was set up as EventEmitter object will emit a new value 
+
+  // + Now I need to pass the value that should be emitted to this emit method ( .emit() )which is provided by EventEmitter object which is stored in select property.
+  // + As an initial value I am going to use user ID that is one of the properties of each object in DUMMY_USER that will be passed through Input decorator (same as avatar and name). I receive it as an input and pass it back to parent component whenever a user is selected
+
+  // ! 2 ! Go to app.component.html.
+
+   
+  
+  onSelectUser() {
+
+    this.select.emit(this.id)
+
+  }
+}
